c92342da4b65ea480172387862569e4b40d26aec
Daniel Hsieh <dan2y006@gmail.com>
2024-06-04 00:01:52 +0800

    refactor: Remove unused code and update page layout class names for RootLayout, Page, and Home components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,6 @@ import { sql } from "@vercel/postgres";
  * If the locale cookie isn't set, it redirects the user to the default one.
  * For the actual content, please visit the "app/[locale]/page.tsx" page component.
  */
-export default async function RootPage({
-  params
-} : {
-  params: { user: string }
-}) : Promise<JSX.Element>{
-//   const res = await sql`CREATE TABLE records (
-// 	record_id INT PRIMARY KEY,
-//   record_datetime datetime NOT NULL,
-//   record_type VARCHAR(100) NOT NULL,
-//   category_id INT FOREIGN KEY NOT NULL,
-//   user_id INT FOREIGN KEY NOT NULL,
-//   description VARCHAR(255),
-//   amount INT NOT NULL,
-//     existed BOOLEAN DEFAULT TRUE;
-// );`;
-// console.log(res);
-  // if(rows.length === 0){
-  //   return redirect("/records");
-  // }
+export default function RootPage() {
   redirect("/records");
 }
