3e5f40588714840adbd171dd7ca9d6084c1904a6
Daniel Hsieh <dan2y006@gmail.com>
2024-06-08 22:39:56 +0800

    refactor: remove unused import in DarkModeToggle component

--- src/components/dark-mode-toggle.tsx
+++ src/components/dark-mode-toggle.tsx
@@ -1,6 +1,8 @@
+"use client"
+
 import * as React from "react"
 import { Moon, Sun } from "lucide-react"
-// import { useTheme } from "next-themes"
+import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -11,7 +13,7 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 export function DarkModeToggle() {
-//   const { setTheme } = useTheme()
+  const { setTheme } = useTheme()
 
   return (
     <DropdownMenu>
@@ -23,7 +25,7 @@ export function DarkModeToggle() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        {/* <DropdownMenuItem onClick={() => setTheme("light")}>
+        <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
@@ -31,7 +33,7 @@ export function DarkModeToggle() {
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
           System
-        </DropdownMenuItem> */}
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   )
