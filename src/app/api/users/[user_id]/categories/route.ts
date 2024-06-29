f75f73d0a95b47d197007bd4b9d26a223aad8b38
Daniel Hsieh <dan2y006@gmail.com>
2024-06-29 13:34:32 +0800

    fix: update NextRequest import in user categories route

--- src/app/api/users/[user_id]/categories/route.ts
+++ src/app/api/users/[user_id]/categories/route.ts
@@ -1,9 +1,8 @@
-import type { NextApiRequest } from 'next'
-import { NextResponse } from "next/server";
+import {  NextResponse, NextRequest } from "next/server";
 import {sql} from "@vercel/postgres";
 
 // To handle a GET request to /api
-export async function GET(req: NextApiRequest, context: { params: { user_id: string } }) {
+export async function GET(req: NextRequest, context: { params: { user_id: string } }) {
   const user_id  = context?.params?.user_id
 
     const { rows: categories } = await sql`SELECT * FROM categories WHERE user_id = ${user_id?.toString()|| `1`};`;
