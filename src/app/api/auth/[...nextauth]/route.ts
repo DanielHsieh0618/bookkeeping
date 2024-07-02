93ddacbd740d0e220480360463e41af77ba49962
Daniel Hsieh <dan2y006@gmail.com>
2024-07-03 02:55:30 +0800

    feat: add login button component for authentication
    - Added a new component called LoginBtn to handle user authentication.
    - The component uses the next-auth/react library for session management.
    - It displays a "Sign in" button when the user is not signed in and a "Sign out" button when the user is signed in.
    - The component also shows the user's email when signed in.

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,24 @@
+
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import type { NextAuthOptions } from "next-auth"
+
+export const authOptions: NextAuthOptions = {
+  // your configs
+  providers:[
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        authorization: {
+        params: {
+            prompt: "consent",
+            access_type: "offline",
+            response_type: "code"
+        }
+      }
+    }),
+  ]
+}
+
+export default NextAuth(authOptions)
+
