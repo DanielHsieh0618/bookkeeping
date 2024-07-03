9f3b7ff28e4939ba78a8a796c5c7cb3c8bff37a6
Daniel Hsieh <dan2y006@gmail.com>
2024-07-04 04:37:07 +0800

    refactor: update auth route to use external authOptions

--- src/app/configs/auth.ts
+++ src/app/configs/auth.ts
@@ -1,4 +1,6 @@
 import type { NextAuthOptions } from "next-auth"
+import NextAuth, { Account, Profile } from "next-auth"
+
 import GoogleProvider from "next-auth/providers/google"
 export const authOptions: NextAuthOptions = {
   // your configs
@@ -7,15 +9,25 @@ export const authOptions: NextAuthOptions = {
         clientId: process.env.GOOGLE_CLIENT_ID as string,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         authorization: {
-        params: {
-            prompt: "consent",
-            access_type: "offline",
-            response_type: "code"
+          params: {
+              prompt: "consent",
+              access_type: "offline",
+              response_type: "code"
+          }
         }
-      }
+        
     }),
   ],
   callbacks: {
+    async signIn({account, profile}:{account: Account | null; profile?: Profile | undefined;}) {
+        const isAccount = account && profile
+        if (isAccount && account.provider === "google") {
+            // return profile.email_verified && profile.email.endsWith("@example.com")
+            return true
+          }
+        return true // Do different verification for other providers that don't have `email_verified`
+    }
   },
+
   secret: process.env.JWT_SECRET || "default-secret"
 }
