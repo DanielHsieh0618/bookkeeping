4acfde2dda77a7e61e7d4e464ff3b85def2ef760
Daniel Hsieh <dan2y006@gmail.com>
2024-07-18 01:06:17 +0800

    chore: Update Next.js API route to use NextRequest and NextResponse

--- src/app/api/userInfo/route.ts
+++ src/app/api/userInfo/route.ts
@@ -1,10 +1,10 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { NextResponse } from "next/server";
+
+import {NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/configs/auth";
 
 // To handle a POST request to /api
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   const session =  await getServerSession(authOptions)
   return NextResponse.json(session, { status: 200 });
 
