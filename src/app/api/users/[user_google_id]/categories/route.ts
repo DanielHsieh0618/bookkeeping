76099aa81213421e35e29daf1d9d09147c991fe6
Daniel Hsieh <dan2y006@gmail.com>
2024-07-18 23:00:17 +0800

    feat: Add logging for user_google_id in categories route

--- src/app/api/users/[user_google_id]/categories/route.ts
+++ src/app/api/users/[user_google_id]/categories/route.ts
@@ -3,6 +3,7 @@ import {sql} from "@vercel/postgres";
 
 // To handle a GET request to /api
 export async function GET(req: NextRequest, context: { params: { user_google_id: string } }) {
+  console.log("context.params.user_google_id", context.params.user_google_id)
   const { rows: users } = await sql`SELECT user_id FROM users WHERE google_id=${context.params.user_google_id}`;
   const user_id = users[0].user_id;
   console.log("user_id", user_id)
